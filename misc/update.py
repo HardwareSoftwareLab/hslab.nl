8e0e1d9048874014e8602d79d72f92c0fab2eadc
Doeke Wartena <clankill3r@gmail.com>
2021-01-21 20:25:37 +0100

    update.py and huge simplification

--- misc/update.py
+++ misc/update.py
@@ -1,10 +1,75 @@
-from create_banners_json import create_banners_json
-from create_hover_descriptions import create_hover_descriptions
+import json
+import os
+from os import listdir
+import os.path
+
+# fuck python
+
+
+def create_banners_json(ignore_folders=[]):
+
+	supported_sizes = [line.strip('\n') for line in open("supported_banner_sizes.txt").readlines()]
+	
+	to_json_data = {}
+	for size in supported_sizes:
+		to_json_data.setdefault(size,[])
+
+	dirs = next(os.walk("../"))[1];
+
+	for folder in dirs:
+		for _, _, files in os.walk("../"+folder):
+
+			data = {}
+			for size in supported_sizes:
+				data.setdefault(size,[])
+
+			for f in files:
+				if not f.startswith("banner_"): continue
+				if "." not in f: continue
+				name_without_extension = f[0:f.rfind(".")]
+
+				for size in supported_sizes:
+					if name_without_extension.endswith(size):
+						data[size].append(f)
+						break
+
+			for size in supported_sizes:
+				if len(data[size]):
+					to_json_data[size].append({
+						"site": folder,
+						"banners": data[size]
+					})
+
+	
+	with open('banners.json', 'w', encoding='utf-8') as out:
+		out.write(json.dumps(to_json_data, sort_keys=True, indent=4))
+		out.close()
+
+
+def create_hover_descriptions(ignore_folders=[]):
+	to_json_data = {}
+	
+	dirs = next(os.walk("../"))[1];
+
+	for folder in dirs:
+
+		if not os.path.exists(f"../{folder}/hover_description.txt"):
+			continue
+
+		f = open(f"../{folder}/hover_description.txt", "r")
+		to_json_data.setdefault(folder, f.read())
+	
+	with open('hover_descriptions.json', 'w', encoding='utf-8') as out:
+		out.write(json.dumps(to_json_data, sort_keys=True, indent=4))
+		out.close()
+
+
 
 def main():
-	create_banners_json()
-	create_hover_descriptions()
-
+	f = open("ignore_folders.txt", "r")
+	ignore_folders = [line.strip('\n') for line in f.readlines()]
+	create_banners_json(ignore_folders)
+	create_hover_descriptions(ignore_folders)
 
 
 if __name__ == "__main__":
