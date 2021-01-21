e0b9bb112bc35d5c92b6601ff10f1f21926c2c4b
Doeke Wartena <clankill3r@gmail.com>
2021-01-21 16:16:54 +0100

    load_banners no longer needs an argument

--- misc/bna.js
+++ misc/bna.js
@@ -4,65 +4,69 @@
 
 
 
-function load_banners(bna) {
+function load_banners() {
 
-	const db = window.localStorage;
+	bna.then(bna => {
 
-	for (let i = 0; i < bna.banners.supported_sizes.length; i++) {
-		const size = bna.banners.supported_sizes[i];
+		const db = window.localStorage;
 
-		const width  = parseInt(size.substring(1, size.indexOf("x")));
-		const height = parseInt(size.substring(size.indexOf("x")+1));
+		for (let i = 0; i < bna.banners.supported_sizes.length; i++) {
+			const size = bna.banners.supported_sizes[i];
 
-		const banners = bna.banners[size];
-		const banner_containers = document.getElementsByClassName("banners"+size);
+			const width  = parseInt(size.substring(1, size.indexOf("x")));
+			const height = parseInt(size.substring(size.indexOf("x")+1));
 
-		let banner_index = db.getItem('banner_index'+size);
-		if (banner_index == null) {
-			// we don't want the choosen banner on a first visit to be the same for 
-			// everyone cause then it will get biased towards one project.
-			banner_index = Math.floor(Math.random()*banners.length);
-		}
-		
-		for (let i = 0; i < banner_containers.length; i++) {
+			const banners = bna.banners[size];
+			const banner_containers = document.getElementsByClassName("banners"+size);
 
-			let banner_data = banners[ banner_index % banners.length ];
+			let banner_index = db.getItem('banner_index'+size);
+			if (banner_index == null) {
+				// we don't want the choosen banner on a first visit to be the same for 
+				// everyone cause then it will get biased towards one project.
+				banner_index = Math.floor(Math.random()*banners.length);
+			}
+			
+			for (let i = 0; i < banner_containers.length; i++) {
 
-			// avoid banners that point to the site we are already on
-			if (`/${banner_data["site"]}/` == document.location.pathname) {
+				let banner_data = banners[ banner_index % banners.length ];
+
+				// avoid banners that point to the site we are already on
+				if (`/${banner_data["site"]}/` == document.location.pathname) {
+					banner_index++;
+					i--;
+					continue;
+				}
+
+				let href = bna.hsl_root+"/"+banner_data["site"];
+				let all_img_urls = banner_data["banners"];
+				let random_img_url = bna.hsl_root+"/"+banner_data["site"]+"/"+all_img_urls[Math.floor(Math.random()*all_img_urls.length)];
+
+				// we also allow .mp4 since it is a way better format then .gif, but it has to be in a <video> element instead of <img>.
+				const is_mp4 = random_img_url.endsWith(".mp4");
+				if (is_mp4) {
+					banner_containers[i].innerHTML = 
+						`<a href="${href}" title="${bna.hover_descriptions[banner_data["site"]]}"> \
+							<video width="${width}" height="${height}" autoplay loop muted> \
+								<source src="${random_img_url}" type="video/mp4"> \
+							</video>
+						</a>`;
+				}
+				else {
+					banner_containers[i].innerHTML = 
+						`<a href="${href}" title="${bna.hover_descriptions[banner_data["site"]]}">` +
+							`<img src="${random_img_url}" width="${width}" height="${height}">`+
+						`</a>`;
+				}
+				banner_containers[i].style["width"] = `${width}px`;
+				banner_containers[i].style["height"] = `${height}px`;
+				banner_containers[i].classList.add("banner");
 				banner_index++;
-				i--;
-				continue;
 			}
 
-			let href = bna.hsl_root+"/"+banner_data["site"];
-			let all_img_urls = banner_data["banners"];
-			let random_img_url = bna.hsl_root+"/"+banner_data["site"]+"/"+all_img_urls[Math.floor(Math.random()*all_img_urls.length)];
-
-			// we also allow .mp4 since it is a way better format then .gif, but it has to be in a <video> element instead of <img>.
-			const is_mp4 = random_img_url.endsWith(".mp4");
-			if (is_mp4) {
-				banner_containers[i].innerHTML = 
-					`<a href="${href}" title="${bna.hover_descriptions[banner_data["site"]]}"> \
-						<video width="${width}" height="${height}" autoplay loop muted> \
-							<source src="${random_img_url}" type="video/mp4"> \
-						</video>
-					</a>`;
-			}
-			else {
-				banner_containers[i].innerHTML = 
-					`<a href="${href}" title="${bna.hover_descriptions[banner_data["site"]]}">` +
-						`<img src="${random_img_url}" width="${width}" height="${height}">`+
-					`</a>`;
-			}
-			banner_containers[i].style["width"] = `${width}px`;
-			banner_containers[i].style["height"] = `${height}px`;
-			banner_containers[i].classList.add("banner");
-			banner_index++;
+			db.setItem('banner_index'+size, banner_index);
 		}
 
-		db.setItem('banner_index'+size, banner_index);
-	}	
+	});	
 }
 
 
@@ -145,9 +149,7 @@ let bna = create_bna();
 	
 	window.addEventListener("DOMContentLoaded", function() {
 		create_css_styles();
-		bna.then(bna => {
-			load_banners(bna);
-		});
+		load_banners();
 	});
 
 })();
