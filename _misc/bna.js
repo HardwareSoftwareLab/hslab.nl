0bcb32ab09f626fa0166265c828da6a775bc5601
Doeke Wartena <clankill3r@gmail.com>
2021-01-23 10:07:35 +0100

    insane huge refactor

--- _misc/bna.js
+++ _misc/bna.js
@@ -1,142 +1,11 @@
 "use strict";
-
 // fuck javascript
 
+let _dbna; // for debugging purposes only!
 
-const create_banner_for_container = function(container, href, hover_description, img_url, width, height) {
+const p_bna = (async function() {
 
-	// we also allow .mp4 since it is a way better format then .gif, but it has to be in a <video> element instead of <img>.
-	const is_mp4 = img_url.endsWith(".mp4");
-	if (is_mp4) {
-		container.innerHTML = 
-			`<a href="${href}" title="${hover_description}"> \
-				<video width="${width}" height="${height}" autoplay loop muted> \
-					<source src="${img_url}" type="video/mp4"> \
-				</video>
-			</a>`;
-	}
-	else {
-		container.innerHTML = 
-			`<a href="${href}" title="${hover_description}">` +
-				`<img src="${img_url}" width="${width}" height="${height}">`+
-			`</a>`;
-	}
-	container.style["width"] = `${width}px`;
-	container.style["height"] = `${height}px`;
-	container.classList.add("banner");
-}
-
-
-
-
-function load_banners() {
-
-	return bna.then(bna => {
-
-		const db = window.localStorage;
-
-		for (let i = 0; i < bna.banners.supported_sizes.length; i++) {
-			const size = bna.banners.supported_sizes[i];
-
-			const width  = parseInt(size.substring(1, size.indexOf("x")));
-			const height = parseInt(size.substring(size.indexOf("x")+1));
-
-			const banners = bna.banners[size];
-			const banner_containers = document.getElementsByClassName("banners"+size);
-
-			let banner_index = db.getItem('banner_index'+size);
-			if (banner_index == null) {
-				// we don't want the choosen banner on a first visit to be the same for 
-				// everyone cause then it will get biased towards one project.
-				banner_index = Math.floor(Math.random()*banners.length);
-			}
-			
-			for (let i = 0; i < banner_containers.length; i++) {
-
-				let container = banner_containers[i];
-
-				// it is possible to create a banner for a specific site
-				// e.g. <div class="banners_128x64" data-site="prolactin"></div>
-				if (container.dataset.site != undefined) {
-
-					let site = container.dataset.site;
-					let href = bna.hsl_root+"/"+site;
-					let hover_description = bna.info_all_sites[site].hover_description;
-					let sized_banners = bna.sites[site].banners[size];
-					let img_url = sized_banners[Math.floor(Math.random()*sized_banners.length)];
-
-					create_banner_for_container(container, href, hover_description, img_url, width, height);
-				}
-				else {
-
-					let banner_data = banners[ banner_index % banners.length ];
-
-					// avoid banners that point to the site we are already on
-					if (`${banner_data.site}` == bna.current_site.site_folder) {
-						banner_index++;
-						i--;
-						continue;
-					}
-
-					let href = bna.hsl_root+"/"+banner_data.site;
-					let all_img_urls = banner_data.banners;
-					let random_img_url = all_img_urls[Math.floor(Math.random()*all_img_urls.length)];
-
-					create_banner_for_container(container, href, bna.info_all_sites[banner_data.site].hover_description, random_img_url, width, height);
-					
-					banner_index++;
-				}
-			}
-
-			db.setItem('banner_index'+size, banner_index);
-		}
-
-
-	});	
-}
-
-
-
-
-const create_css_styles = function() {
-	var sheet = document.createElement('style')
-	sheet.innerHTML = 
-		`.banner {
-			border: 1px solid black;
-		}`;
-	document.body.appendChild(sheet);
-}
-
-
-const set_html_title = function() {
-	bna.then(bna => {
-		if (document.title == "") {
-			document.title = bna.current_site.title;	
-		}
-	});
-}
-
-
-const create_bna = async function() {
-
-	let bna = {
-		"banners": {
-			// @Banner_Support_Sizes
-			"supported_sizes": ["_512x128", "_128x512", "_128x64"]
-		},
-		"sites": {
-		},
-		"current_site": {
-			"site_folder": (function() {
-				let path = document.location.pathname;
-				path = path.substring(0, path.length-1);
-				return path.substring(path.lastIndexOf("/")+1);
-			})()
-		}
-	};
-
-	bna.hsl_root = (function() {
-
+	const hsl_root = (function() {
 		// Some people point their localhost to a folder where multiple sites live.
 		// This can give issues with loading files. We deal with that here.
 		if (document.location.href.includes('localhost') || document.location.href.includes('127.0.0.1')) {
@@ -150,68 +19,192 @@ const create_bna = async function() {
 	})();
 
 
-	let files = [];
-	const resources = [
-		bna.hsl_root+"/_misc/info_all_sites.json", 
-		bna.hsl_root+"/_misc/banners.json"
-	];
-	resources.forEach(it => {
-		files.push(fetch(it))
-	});
+	//
+	// data.json
+	//
+	const data = {
+		...{"hsl_root": hsl_root}, 
+		... await (await fetch(hsl_root+"/_misc/data.json")).json()
+	}
 
-	
-	files = await Promise.all(files);
+	//
+	// complete the banner sources so it's a full link
+	// and set href for site
+	//
+	for (const site in data.sites) {
+		data.supported_banner_sizes.forEach(size => {
+			const arr = data.sites[site].banner_sources[size];
+			for (const i in arr) {
+				arr[i] = `${hsl_root}/${site}/${arr[i]}`;
+			}
+		});
+		data.sites[site].href = `${hsl_root}/${site}`;
+	}
 
-	let json_promises = [];
-	files.forEach(it => {
-		json_promises.push(it.json());
-	});
+	//
+	// set data.current_site
+	//
+	data.current_site = (() => {
+		let path = document.location.pathname;
+		path = path.substring(0, path.length-1);
+		path = path.substring(path.lastIndexOf("/")+1);
+		return data.sites[path];
+	})();
 
-	return Promise.all(json_promises).then(jsons => {
-		bna.info_all_sites = jsons[0];
-		bna.banners = Object.assign(jsons[1], bna.banners);
-
-		//
-		// fill in bna.sites
-		//
-		for (let i = 0; i < bna.banners.supported_sizes.length; i++) {
-			const size = bna.banners.supported_sizes[i];
-
-			for (let j = 0; j < bna.banners[size].length; j++) {
-
-				let sized_banners = bna.banners[size][j];
-
-				for (let k = 0; k < sized_banners.banners.length; k++) {
-					sized_banners.banners[k] = bna.hsl_root+"/"+sized_banners.banners[k];
-				}
-
-				if (bna.sites[sized_banners.site] == undefined) {
-					bna.sites[sized_banners.site] = {"banners": {}};
-				}
-				bna.sites[sized_banners.site].banners[size] = sized_banners.banners;	
+	//
+	// data.sites_that_have_banner_size_x
+	//
+	data.sites_that_have_banner_size_x = {};
+	data.supported_banner_sizes.forEach(size => {
+		data.sites_that_have_banner_size_x[size] = [];
+		for (const site in data.sites) {
+			if (data.sites[site].banner_sources[size]) {
+				data.sites_that_have_banner_size_x[size].push(data.sites[site]);
 			}
 		}
-
-		bna.current_site = Object.assign(bna.info_all_sites[bna.current_site.site_folder], bna.current_site);
-		bna.current_site = Object.assign(bna.sites[bna.current_site.site_folder], bna.current_site);
-
-		dbna = bna;
-
-		return bna;
 	});
+	
 
-};
+	//
+	// functions
+	//
+
+	const create_css_styles = function() {
+		var sheet = document.createElement('style')
+		sheet.innerHTML = 
+			`.banner {
+				border: 1px solid black;
+			}`;
+		document.body.appendChild(sheet);
+	}
 
 
-let bna = create_bna();
-let dbna; // for debug purposes in the console only, since bna is a Promise and they are hard to work with in the console (imposible?)
+	const set_html_title = function() {
+		if (document.title == "") {
+			document.title = data.current_site.title;	
+		}
+	}
+
+
+	const create_banner_for_container = function(container, href, hover_description, img_url, width, height) {
+
+		// we also allow .mp4 since it is a way better format then .gif, but it has to be in a <video> element instead of <img>.
+		const is_mp4 = img_url.endsWith(".mp4");
+		if (is_mp4) {
+			container.innerHTML = 
+				`<a href="${href}" title="${hover_description}"> \
+					<video width="${width}" height="${height}" autoplay loop muted> \
+						<source src="${img_url}" type="video/mp4"> \
+					</video>
+				</a>`;
+		}
+		else {
+			container.innerHTML = 
+				`<a href="${href}" title="${hover_description}">` +
+					`<img src="${img_url}" width="${width}" height="${height}">`+
+				`</a>`;
+		}
+		container.style["width"] = `${width}px`;
+		container.style["height"] = `${height}px`;
+		container.classList.add("banner");
+	}
+
+
+
+	function load_banners() {
+
+		const db = window.localStorage;
+
+		for (let i = 0; i < data.supported_banner_sizes.length; i++) {
+			const size = data.supported_banner_sizes[i];
+
+			const width  = data.width_height_lookup[size][0];
+			const height = data.width_height_lookup[size][1];
+
+			const banner_containers = document.getElementsByClassName("banners"+size);
+
+			for (let i = 0; i < banner_containers.length; i++) {
+				let container = banner_containers[i];
+
+				// it is possible to create a banner for a specific site
+				// e.g. <div class="banners_128x64" data-site="prolactin"></div>
+				if (container.dataset.site != undefined) {
+
+					const _site = container.dataset.site;
+					const site = data.sites[_site];
+					const all_img_urls = site.banner_sources[size];
+					const random_img_url = all_img_urls[Math.floor(Math.random()*all_img_urls.length)];
+
+					create_banner_for_container(container, 
+						site.href, site.hover_description, 
+						random_img_url, width, height);
+				}
+				else {
+					// next banner that fits
+					const sites = data.sites_that_have_banner_size_x[size];
+					
+					let index = db.getItem('index_for_size'+size);
+					if (index == null) {
+						// we don't want the choosen banner on a first visit to be the same for 
+						// everyone cause then it will get biased towards one project.
+						index = Math.floor(Math.random()*sites.length);
+					}
+
+					// avoid banners that point to the site we are already on
+					if (data.current_site.folder == sites[index].folder) {
+						index++;
+						index %= sites.length;
+					}
+
+					const site = sites[index];
+					
+					const all_img_urls = site.banner_sources[size];
+					const random_img_url = all_img_urls[Math.floor(Math.random()*all_img_urls.length)];
+
+					create_banner_for_container(container, 
+						site.href, site.hover_description, 
+						random_img_url, width, height);
+					
+
+					index++;
+					index %= sites.length;
+					db.setItem('index_for_size'+size, index);
+				}
+
+			}
+		}
+	}
+
+
+	//
+	// library
+	//
+
+	const result = {
+		...data, 
+		"create_css_styles": create_css_styles,
+		"set_html_title": set_html_title,
+		"load_banners": load_banners,
+	}
+
+	_dbna = result;
+
+	return result;
+
+})();
+
 
 (function(){
 	
-	window.addEventListener("DOMContentLoaded", function() {
-		create_css_styles();
-		load_banners();
-		set_html_title();
+	window.addEventListener("DOMContentLoaded", async function() {
+
+		const bna = await p_bna;
+
+		bna.create_css_styles();
+		bna.set_html_title();
+		bna.load_banners();
+		
 	});
 
 })();
+
--- _misc/update.py
+++ _misc/update.py
@@ -2,108 +2,113 @@ import json
 import os
 from os import listdir
 import os.path
+from pprint import pprint
 
 # fuck python
 
 
-def first_check_if_folder_is_valid_for_site(folder, ignore_folders=[]) -> bool:
+def create_dict_banner_sources(folder, supported_sizes) -> ():
 
-	if folder in ignore_folders:
-		return False
+	banner_sources = {}
 
-	# site_info.json is mandatory
-	if not os.path.exists(f"../{folder}/site_info.json"):
-		return False
+	for _, _, files in os.walk(folder):
 
-	return True
-
-
-
-
-def create_banners_json(ignore_folders=[]):
-
-	supported_sizes = [line.strip('\n') for line in open("supported_banner_sizes.txt").readlines()]
-	
-	to_json_data = {}
-	for size in supported_sizes:
-		to_json_data.setdefault(size,[])
-
-	dirs = next(os.walk("../"))[1];
-
-	for folder in dirs:
-
-		if not first_check_if_folder_is_valid_for_site(folder, ignore_folders):
-			continue
-
-		for _, _, files in os.walk("../"+folder):
-
-			unfound_sizes = supported_sizes.copy()
+		unfound_sizes = supported_sizes.copy()
 		
-			data = {}
+		data = {}
+		for size in supported_sizes:
+			data.setdefault(size,[])
+
+		for f in files:
+			if not f.startswith("banner_"): continue
+			if "." not in f: continue
+			name_without_extension = f[0:f.rfind(".")]
+
 			for size in supported_sizes:
-				data.setdefault(size,[])
-
-			for f in files:
-				if not f.startswith("banner_"): continue
-				if "." not in f: continue
-				name_without_extension = f[0:f.rfind(".")]
-
-				for size in supported_sizes:
-					if name_without_extension.endswith(size):
-						data[size].append(folder+"/"+f)
-						unfound_sizes.remove(size)
-						break
-
-			if len(unfound_sizes):
-				print(f"{folder} has the following missing banners: {unfound_sizes}")
-				# should all bannders be required?
-			
-			for size in supported_sizes:
-				if len(data[size]):
-					to_json_data[size].append({
-						"site": folder,
-						"banners": data[size]
-					})
-
-	
-	with open('banners.json', 'w', encoding='utf-8') as out:
-		out.write(json.dumps(to_json_data, sort_keys=True, indent=4))
-		out.close()
-
-
-
-def create_info_all_sites_json(ignore_folders=[]):
-	to_json_data = {}
-	
-	dirs = next(os.walk("../"))[1];
-
-	for folder in dirs:
-
-		if not first_check_if_folder_is_valid_for_site(folder, ignore_folders):
-			continue
-
-		with open(f"../{folder}/site_info.json") as json_file:
-			data = json.load(json_file)
-			to_json_data[folder] = data
-
-	with open('info_all_sites.json', 'w', encoding='utf-8') as out:
-		out.write(json.dumps(to_json_data, sort_keys=True, indent=4))
-		out.close()
+				if name_without_extension.endswith(size):
+					data[size].append(f)
+					unfound_sizes.remove(size)
+					break
+		
+		for size in supported_sizes:
+			if len(data[size]):
+				banner_sources[size] = data[size];
+				
+	return (banner_sources, unfound_sizes)
 
 
 
 def main():
-	print('update...')
-	f = open("ignore_folders.txt", "r")
-	ignore_folders = [line.strip('\n') for line in f.readlines()]
-	print(f'{"-"*20}\nignoring folders:\n')
-	for folder in ignore_folders: print(folder)
-	print(f'{"-"*20}')
-	print('create_banners_json...')
-	create_banners_json(ignore_folders)
-	print('create_info_all_sites_json...')
-	create_info_all_sites_json(ignore_folders)
-	print('done')
+
+	json_file = {
+		"sites": {}
+	}
+
+	errors = []
+
+	with open('update_info.json') as f:
+		update_info = json.load(f)
+
+		json_file = {**json_file, **update_info}
+
+		dirs = next(os.walk("../"))[1];
+
+		for folder in dirs:
+			if folder in update_info["ignore_folders"]:
+				continue
+			if not os.path.exists(f"../{folder}/site_info.json"):
+				continue
+
+			#
+			# banner sources
+			#
+			banner_sources, unfound_sizes = create_dict_banner_sources(f"../{folder}", update_info["supported_banner_sizes"])
+
+			ok = True
+
+			if len(unfound_sizes):
+				error_prefix = "[ERROR]" if update_info["one_banner_of_each_size_required"] else "[WARNING]"
+				errors.append(f"{error_prefix} '{folder}' has no banners in the following sizes {unfound_sizes}")
+				if update_info["one_banner_of_each_size_required"]:
+					ok = False
+
+			if not ok: continue
+			
+			#
+			# data from site_info.json
+			#
+			with open(f"../{folder}/site_info.json") as f2:
+				site_info = {**json.load(f2), **{"folder": folder}}
+
+			site_data = {
+				**site_info,
+				**{"banner_sources": banner_sources}
+			}
+
+			json_file["sites"] = {**json_file["sites"], **{folder: site_data}}
+
+		#
+		# width_height_lookup
+		#
+		width_height_lookup = {}
+
+		for size in update_info["supported_banner_sizes"]:
+			wh = size[1:].split("x")
+			width_height_lookup[size] = [int(wh[0]),int(wh[1])]
+		
+		json_file["width_height_lookup"] = width_height_lookup
+		
+		#
+		# export to data.json
+		#
+		with open('data.json', 'w', encoding='utf-8') as out:
+			out.write(json.dumps(json_file, sort_keys=True, indent=4))
+			out.close()
+
+	for error in errors:
+		print(error)
+
+	print("done")
 
 
 if __name__ == "__main__":
